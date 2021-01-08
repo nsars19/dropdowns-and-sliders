4fed43474c45f1b1df7896dc1dd32584b3013555
Nick <nsardonini@gmail.com>
2021-01-08 14:55:18 -0500

    add display toggling function, move event listener logic to bottom of function declaration

--- src/index.js
+++ src/index.js
@@ -3,18 +3,22 @@ import "./stylesheets/css/main.css";
 const app = (() => {
   const $ = (elem) => document.querySelectorAll(elem);
 
-  [...$("navitem")].forEach((element) => {
-    element.addEventListener("click", () => {
-      element.childNodes[3].classList.toggle("display-off");
-    });
-
-    let children = [...element.childNodes[3].children[0].children];
-    children.forEach((child) => addListenerToChild(child));
-  });
-
   function addListenerToChild(child) {
     child.addEventListener("click", () => {
-      console.log(child);
+      // do something with child element
     });
   }
+
+  function toggleDisplay(elem) {
+    elem.classList.toggle("display-off");
+  }
+
+  [...$("navitem")].forEach((element) => {
+    element.addEventListener("click", () =>
+      toggleDisplay(element.childNodes[3])
+    );
+
+    const children = [...element.childNodes[3].children[0].children];
+    children.forEach((child) => addListenerToChild(child));
+  });
 })();
